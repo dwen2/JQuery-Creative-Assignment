6f1b0bb18d001672c926c7bf8a10437498cf78c0
Dezhang Wen <dezhang12@gmail.com>
2018-10-05 04:30:44 +0000

    changes

--- dogs.js
+++ dogs.js
@@ -2,19 +2,19 @@
   var myurl = "https://dog.ceo/api/breeds/image/random";
   console.log(myurl);
   $.ajax({
-   type: 'GET', 
+   type: 'GET',
    url: myurl,
    crossDomain: true,
    dataType: "json",
    success: function(parsed_json) {
     console.log(parsed_json);
-    var image = parsed_json['message']; 
-    console.log(image); 
-    
+    var image = parsed_json['message'];
+    console.log(image);
+
     var img = document.createElement("img");
     img.src = image;
-    console.log(img.src); 
-    
+    console.log(img.src);
+
     $("#dogs").html(img);
    }
   });
