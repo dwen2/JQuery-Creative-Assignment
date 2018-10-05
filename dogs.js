4d3e2866c81c93cb567949107927fcee230fe808
Dezhang Wen <dezhang12@gmail.com>
2018-10-05 04:20:31 +0000

    changes

---
+++ dogs.js
@@ -0,0 +1,22 @@
+ $("#btn").click(function(e) {
+  var myurl = "https://dog.ceo/api/breeds/image/random";
+  console.log(myurl);
+  $.ajax({
+   type: 'GET', 
+   url: myurl,
+   crossDomain: true,
+   dataType: "json",
+   success: function(parsed_json) {
+    console.log(parsed_json);
+    var image = parsed_json['message']; 
+    console.log(image); 
+    
+    var img = document.createElement("img");
+    img.src = image;
+    console.log(img.src); 
+    
+    $("#dogs").html(img);
+   }
+  });
+ });
+ 
